1e7823f68fb5ea5f66dd4f6373a75bb54881b6f6
markoarsyc <markoarsyc@gmail.com>
2024-07-30 14:26:01 +0200

    Dodate su sve funkcionalnosti za rad sa komentarima

--- src/AddNew.js
+++ src/AddNew.js
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 import "./Styles/AddNew.css";
 
-const AddNew = ({onAdd}) => {
-  const [cartoon, setCartoon] = useState(
-    {title:'', year:"",img:""}
-  );
+const AddNew = ({ onAdd }) => {
+  const [cartoon, setCartoon] = useState({ title: "", year: "", img: "" });
 
   //controlled form input
   const handleChange = (e) => {
-    setCartoon({...cartoon, [e.target.name] : e.target.value});
-  }
+    setCartoon({ ...cartoon, [e.target.name]: e.target.value });
+  };
 
   //adding new cartoon
-  const handleSubmit = async (e) =>  {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3003/cartoons",{
-        method:"POST",
+      const response = await fetch("http://localhost:3003/cartoons", {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(cartoon)
+        body: JSON.stringify(cartoon),
       });
       if (response.ok) {
         onAdd(await response.json());
@@ -31,20 +29,36 @@ const AddNew = ({onAdd}) => {
     } catch (error) {
       console.error("Error adding cartoon:", error);
     }
-
-    
-  }
+  };
 
   return (
     <div className="add-new">
       <h2> Add new memory! </h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="TITLE"> Title: </label>
-        <input type="text" id="TITLE" name="title" value={cartoon.title} onChange={handleChange}/>
+        <input
+          type="text"
+          id="TITLE"
+          name="title"
+          value={cartoon.title}
+          onChange={handleChange}
+        />
         <label htmlFor="YEAR"> Year: </label>
-        <input type="text" id="YEAR" name="year" value={cartoon.year} onChange={handleChange}/>
+        <input
+          type="text"
+          id="YEAR"
+          name="year"
+          value={cartoon.year}
+          onChange={handleChange}
+        />
         <label htmlFor="IMG"> Image URL: </label>
-        <input type="text" id="IMG" name="img" value={cartoon.img} onChange={handleChange}/>
+        <input
+          type="text"
+          id="IMG"
+          name="img"
+          value={cartoon.img}
+          onChange={handleChange}
+        />
         <button type="submit"> Submit </button>
       </form>
     </div>
--- src/Cartoon.js
+++ src/Cartoon.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import "./Styles/Cartoon.css";
 
-const Cartoon = ({ title, img, year, review, cid, onRemove }) => {
+const Cartoon = ({
+  title,
+  img,
+  year,
+  review: initialReview,
+  cid,
+  onRemove,
+}) => {
   //props must be written as object in {} --> destructuring
 
   const defaultImageUrl =
@@ -10,6 +17,8 @@ const Cartoon = ({ title, img, year, review, cid, onRemove }) => {
   let [btnText, setBtnText] = useState("Show review");
   let [isShown, setIsShown] = useState(false);
   let [imgSrc, setImgSrc] = useState(img || defaultImageUrl);
+  let [reviewText, setReviewText] = useState("");
+  let [review, setReview] = useState(initialReview);
 
   const handleRemove = async () => {
     try {
@@ -26,6 +35,50 @@ const Cartoon = ({ title, img, year, review, cid, onRemove }) => {
     }
   };
 
+  const handleAddReview = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:3003/cartoons/${cid}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ review: reviewText }),
+      });
+      if (response.ok) {
+        const updatedCartoon = await response.json(); // Dodato da se dobije ažurirani objekat
+        setReview(updatedCartoon.review); // Ažuriranje stanja sa novom recenzijom
+        setReviewText("");
+        setIsShown(true);
+        setBtnText("Hide review");
+      } else {
+        console.log("Error adding review");
+      }
+    } catch (error) {
+      console.log("Error adding review");
+    }
+  };
+
+  const handleDeleteReview = async () => {
+    try {
+      const response = await fetch(`http://localhost:3003/cartoons/${cid}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ review: "" }),
+      });
+      if (response.ok) {
+        setReview(""); // Ažuriranje stanja sa praznom recenzijom
+        setIsShown(false);
+      } else {
+        console.log("Error deleting review");
+      }
+    } catch (error) {
+      console.log("Error deleting review");
+    }
+  };
+
   return (
     <article className="cartoon">
       <h2> {title} </h2>
@@ -36,23 +89,53 @@ const Cartoon = ({ title, img, year, review, cid, onRemove }) => {
       />
       <p> {year} </p>
       <div className="options">
-        <button
-          className="show-review"
-          onClick={() => {
-            btnText === "Show review"
-              ? setBtnText("Hide review")
-              : setBtnText("Show review");
-            setIsShown(!isShown);
-          }}
-        >
-          {" "}
-          {btnText}{" "}
-        </button>
+        {review && (
+          <button
+            className="show-review"
+            onClick={() => {
+              btnText === "Show review"
+                ? setBtnText("Hide review")
+                : setBtnText("Show review");
+              setIsShown(!isShown);
+            }}
+          >
+            {" "}
+            {btnText}{" "}
+          </button>
+        )}
+
         <button className="remove-btn" onClick={handleRemove}>
-          Remove
+          Remove cartoon
         </button>
       </div>
-      <div className="review">{isShown && <p> {review} </p>}</div>
+      <div className="review-prompt">
+        <form onSubmit={handleAddReview}>
+          <label htmlFor="review">Review:</label>
+          <input
+            id="review"
+            type="text"
+            value={reviewText}
+            name="review"
+            onChange={(e) => {
+              setReviewText(e.target.value);
+            }}
+          />
+          <button type="submit">Add review</button>
+        </form>
+      </div>
+      <div className="review">
+        {isShown && (
+          <div>
+            <p> {review} </p>
+            {review && (
+              <button className="remove-btn" onClick={handleDeleteReview}>
+                {" "}
+                Delete review{" "}
+              </button>
+            )}
+          </div>
+        )}
+      </div>
     </article>
   );
 };
